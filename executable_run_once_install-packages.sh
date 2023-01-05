c5d9dfbad935738431c37ad38b9c520acaed5142
jsupratman13 <supratmanjoshua@gmail.com>
2023-01-05 14:27:26 +0900

    fix bug

--- executable_run_once_install-packages.sh
+++ executable_run_once_install-packages.sh
@@ -1,4 +1,4 @@
-#!/bin sh
+#!/bin/sh
 sudo apt update
 sudo apt install vim fonts-font-awesome
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
